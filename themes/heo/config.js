f346bbdcbec10c557a2621687528bae5cbbd1e7d
momeigui <momeigui@gmail.com>
2024-01-10 13:56:46 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -67,7 +67,7 @@ const CONFIG = {
     },
     {
       title_1: 'Swift',
-      img_1: '/images/heo/202328bbee0b314297917b327df4a704db5c072402.webp',
+      img_1: '/images/heo/2023e4058a91608ea41751c4f102b131f267075902.webp',
       color_1: '#eb6840',
       title_2: 'Principle',
       img_2: '/images/heo/2023f76570d2770c8e84801f7e107cd911b5073202.webp',
